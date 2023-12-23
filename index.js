2d69cc599d079582e9fe42ae0bcd93cb85c48231
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-23 17:30:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName ( name1 ){
+    result = name1.toUpperCase();
+    return result;
+}
+upperCaseCustomerName ( customerName );
+
+
+
+function setBestCustomer (){
+    bestCustomer = "not bob";
+}
+setBestCustomer ();
+
+function overwriteBestCustomer (){
+    bestCustomer = "maybe bob";
+}
+overwriteBestCustomer ();
+console.log ( bestCustomer );
+
+
+const leastFavoriteCustomer = "Perez";
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = "Bob";
+}
+changeLeastFavoriteCustomer();
+console.log ( leastFavoriteCustomer );
+
